46ad2a8d1d6bd0a88710c48d2fab76634d7dabf2
Gorka <4787906+gorka@users.noreply.github.com>
2023-09-11 19:03:09 -0500

    Fixes (#12)
    * Cannot get all the receives invitations with a lambda 🤷‍♂️
    
    * Assign invitation to user when accepting if it was created for a non registered user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,15 @@ class User < ApplicationRecord
   has_many :campaigns, through: :memberships
   has_many :sessions, through: :campaigns, source: :sessions
   has_many :sent_invitations, class_name: "Invitation", foreign_key: "owner_id"
-  has_many :received_invitations, class_name: "Invitation", foreign_key: "user_id"
+  # has_many :received_invitations, -> (object) { where("invitations.email": object.email) }, class_name: "Invitation"
 
   validates_presence_of :username
 
   def has_pending_invitations?
     received_invitations.pending.any?
+  end
+
+  def received_invitations
+    Invitation.where(email: email)
   end
 end
